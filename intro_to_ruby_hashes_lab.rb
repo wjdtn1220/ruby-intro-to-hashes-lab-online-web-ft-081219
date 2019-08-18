4140a623ef7a5d53d3cb5760b392efac2e8e338a
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 23:28:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,13 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :names => {}
+      :rent_in_dollars => {}
+    }
+	}
 end
 
 def monopoly_with_third_tier
