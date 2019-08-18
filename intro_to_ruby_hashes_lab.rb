3995433aeac95ce57af570c884d41febf86f3dc0
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 23:26:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,34 +7,34 @@ def actor
 end
 
 def monopoly
-	monopoly = {
-	  :railroads => {}
+monopoly = {
+  :railroads => {}
 	}
 end
 
 def monopoly_with_second_tier
-	monopoly = {
-	  :railroads => {
-	    :pieces => {}
-	    :names => {}
-	    :rent_in_dollars => {}
+monopoly = {
+  :railroads => {
+  :pieces => {}
+  :names => {}
+  :rent_in_dollars => {}
 	  }
 	}
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-	  :railroads => {
-	    :pieces => {}
-	    :names => {}
-	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
-	    }
-	  }
-	}
+monopoly = {
+  :railroads => {
+    :pieces => {}
+    :names => {}
+    :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_pieces_owned => 50,
+	    :three_pieces_owned => 100,
+	    :four_pieces_owned => 200
+    }
+  }
+}
 end
 
 def monopoly_with_fourth_tier
